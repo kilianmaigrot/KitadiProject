091d6ad22b2ec5a5ea6145a41646802607107bb7
unknown <marcillac.formation@gmail.com>
2023-10-31 15:41:09 +0100

    Jb

--- www/tailwind.config.js
+++ www/tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
+  content: [
+    "./**/*.{html,js}", 
+    "./node_modules/flowbite/**/*.js"
+  ],
   theme: {
     extend: {},
   },
-  plugins: [require('flowbite/plugin')],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
