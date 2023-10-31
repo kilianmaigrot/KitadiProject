0762ff7bd8299f4c2ecb388547a7bda320416b71
Kilian Maigrot <kilian.maigrot@gmail.com>
2023-10-31 10:11:19 +0100

    Kilian matin 31/10

--- www/js/accueil.js
+++ www/js/accueil.js
@@ -1,16 +1,24 @@
 let pageBody = document.getElementById("body")
 
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+
+    myDBFormation = window.sqlitePlugin.openDatabase({ name: "Plombiers.db", location: 'default' });
+
+}
+
 function loadAccueil () {
 
     pageBody.innerHTML = `
-    <main class="flex flex-col p-4 bg-slate-300 min-h-screen text-lime-900 font-semibold">
-        <img src="./img/KitadiLogo.png" class="mb-8" />
+    <main class="flex flex-col p-4 bg-slate-300 min-h-screen text-lime-900 font-semibold text-base md:text-2xl">
+        <img class="mx-auto mb-8" src="./img/KitadiLogo.png" />
         <h1 class="text-center mb-8">Bienvenue sur l'application Kitadi Energies de calcul de la puissance énergétique d'une Maison.</h1>
         <button class="bg-slate-400 font-bold py-2 px-4 rounded mb-8">
             Créer un nouveau dossier de calcul
         </button>
         <div class="relative overflow-x-auto">
-            <table id="tableProjets" class="w-full text-sm text-gray-400 text-center">
+            <table id="tableProjets" class="w-full text-sm md:text-2xl text-gray-400 text-center">
                 <thead class="text-xs text-gray-400 uppercase bg-gray-700">
                     <tr>
                         <th scope="col" class="px-2 py-3">
--- www/tailwind.config.js
+++ www/tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./**/*.{html,js}",],
+    "./**/*.{html,js}", 
+    "./node_modules/flowbite/**/*.js"
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
